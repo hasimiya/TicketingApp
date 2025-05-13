2bac4d8f137078dd8783897c906072b1e406b340
Віталій Посохов <hachhook@gmail.com>
2025-05-13 20:13:07 +0300

    Add project files.

---
+++ TicketingApp/Components/HeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketingApp.Components
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("Default");
+        }
+    }
+}
