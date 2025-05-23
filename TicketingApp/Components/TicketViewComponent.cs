14f231e8a0310f0249b84157b9a6aa96716f9504
Віталій Посохов <hachhook@gmail.com>
2025-05-23 07:24:01 +0300

    23.05.25

--- TicketingApp/Components/TicketViewComponent.cs
+++ TicketingApp/Components/TicketViewComponent.cs
@@ -5,10 +5,25 @@ namespace TicketingApp.Components
 {
     public class TicketViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(string typeTicket)
         {
-            var ticket = TicketRepository.GetAllTicket();
-            return View(ticket);
+            var allTicket = TicketRepository.GetAllTicket();
+            var openedTicket = TicketRepository.GetOpenedTicket();
+            var solvedTicket = TicketRepository.GetSolvedTicket();
+
+            if (string.IsNullOrEmpty(typeTicket) || typeTicket == "AllTicket")
+            {
+                return View(allTicket);
+            }
+            else if (typeTicket == "OpenedTicket")
+            {
+                return View(openedTicket);
+            }
+            else if (typeTicket == "SolvedTicket")
+            {
+                return View(solvedTicket);
+            }
+            return View();
         }
     }
 }
